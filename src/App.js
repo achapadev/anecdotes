19450f42131079c7207e344657bf77fd7967ecd6
Adrian Chapa <achapa@adrianchapa.dev>
2021-08-27 19:31:25 -0500

    app using everything learned so far, UseState with array etc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>
 }
 
-export default App;
+const MostVotes = ({ array, anecdotes }) => {
+  let max = Math.max(...array)
+  let indexOfMaxValue = array.indexOf(Math.max(...array))
+  return (
+    <div>
+      {anecdotes[indexOfMaxValue]}
+      <div>has {max} votes</div>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+  console.log(points)
+
+  const handleClicked = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const handleClickedVote = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      {anecdotes[selected]}
+      <div>has {points[selected]} votes</div>
+      <div>
+        <Button onClick={handleClickedVote} text="vote" />
+        <Button onClick={handleClicked} text="next anecdote" />
+      </div>
+      <h2>Anecdote with most votes</h2>
+      <MostVotes array={points} anecdotes={anecdotes} />
+    </div>
+  )
+}
+
+export default App
